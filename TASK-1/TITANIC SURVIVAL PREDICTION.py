ebc28ea202ca7f27f7272de92b8f1cccebcbcaf6
SujanKutty <154114096+SujanKutty@users.noreply.github.com>
2024-06-13 17:54:02 +0530

    TITANIC SURVIVAL PREDICTION.py

--- TASK-1/TITANIC SURVIVAL PREDICTION.py
+++ TASK-1/TITANIC SURVIVAL PREDICTION.py
@@ -1,1 +1,47 @@
 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+titanic_df = pd.read_csv("CodSoft\Task1\Titanic-Dataset.csv")
+
+print(titanic_df)
+
+titanic_df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+titanic_df['Age'] = titanic_df['Age'].fillna(titanic_df['Age'].median())
+titanic_df['Embarked'] = titanic_df["Embarked"].fillna(titanic_df['Embarked'].mode()[0]) 
+
+le = LabelEncoder()
+titanic_df['Sex'] = le.fit_transform(titanic_df['Sex'])
+titanic_df['Embarked'] = le.fit_transform(titanic_df['Embarked'])
+
+X = titanic_df.drop('Survived', axis=1)
+y = titanic_df['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+print('Classification Report:')
+print(classification_report(y_test, y_pred))
+print('Confusion Matrix:')
+print(confusion_matrix(y_test, y_pred))
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
